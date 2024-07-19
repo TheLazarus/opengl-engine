3b21f9e857d0423a7f55a41b43c73fce6cfcfce9
Sarthak <ss22072000@gmail.com>
2024-07-19 16:36:44 +0530

    Initializing OpenGL window

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+#include <iostream>
+
+int main()
+{
+
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *window = glfwCreateWindow(800, 600, "My First OpenGL Window", NULL, NULL);
+
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW Window" << "\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    return 0;
+}
